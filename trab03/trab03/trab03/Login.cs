0dd7f6710f52788b2154d47e24e3ebd421669bb3
fellipe romano <fe.soaresromano@gmail.com>
2021-11-30 22:29:36 -0300

    upload

---
+++ trab03/trab03/trab03/Login.cs
@@ -0,0 +1,23 @@
+﻿//FELLIPE SOARES ROMANO CB3005411
+//STEFANY LISBOA DA SILVA CB3005551
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trab03
+{
+    class Login
+    {
+        public string cep { get; set; }
+        public string logradouro { get; set; }
+        public string complemento { get; set; }
+        public string bairro { get; set; }
+        public string localidade { get; set; }
+        public string uf { get; set; }
+        public string ibge { get; set; }
+        public string gia { get; set; }
+        public string ddd { get; set; }
+        public string siafi { get; set; }
+    }
+}
---
+++ trab03/trab03/trab03/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿//FELLIPE SOARES ROMANO CB3005411
+//STEFANY LISBOA DA SILVA CB3005551
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+
+namespace trab03
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void Res_Clicked(object sender, EventArgs e)
+        {
+
+
+            
+
+            try
+            {
+                var mainpage = new MainPage();
+                var httpClient = new HttpClient();
+                var reponse = await httpClient.GetStringAsync("https://viacep.com.br/ws/01001000/json/");
+                var login = JsonConvert.DeserializeObject<Login>(reponse);
+                mainpage.BindingContext = login;
+                cep.Text = "CEP: "+login.cep;
+                logradouro.Text = "Logradouro: "+login.logradouro;
+                complemento.Text = "Complemento: "+login.complemento;
+                bairro.Text = "Bairro: "+login.bairro;
+                localidade.Text = "Localidade: "+login.localidade;
+                uf.Text = "UF: "+login.uf;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops, ocorreu um erro...", ex.Message, "OK");
+            }
+        }
+    }
+}
